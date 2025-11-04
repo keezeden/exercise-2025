b1227f204a8546b529bc7cc37e191d2ce32049dc
keezeden <keezeden@gmail.com>
2025-11-04 14:20:57 +1300

    Update core getUserById function instead

--- src/lib/data/users.ts
+++ src/lib/data/users.ts
@@ -1,29 +1,23 @@
 import { db } from "@/lib/db";
 import { users, posts } from "@/lib/db/schema";
 import { eq } from "drizzle-orm";
-import { Post, User } from "../db/types";
-
-type UserWithPosts = User & {
-  posts: Post[];
-};
 
 export async function getUserById(userId: number) {
   const [user] = await db.select().from(users).where(eq(users.id, userId)).limit(1);
-  return user;
+
+  const { hashedPassword, ...userWithoutHash } = user;
+
+  return userWithoutHash;
 }
 
-export async function getUserWithPosts(userId: number): Promise<UserWithPosts | null> {
+export async function getUserWithPosts(userId: number) {
   const user = await getUserById(userId);
   if (!user) return null;
 
   const userPosts = await db.select().from(posts).where(eq(posts.authorId, userId));
 
-  const { id, username, createdAt } = user;
-
   return {
-    id,
-    username,
-    createdAt,
+    ...user,
     posts: userPosts,
   };
 }
